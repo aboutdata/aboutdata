b9422b285b6e2957b2c5fa32c76a0368873cae90
一名清官 <aboutdata@vip.qq.com>
2015-10-28 16:00:27 +0800

    grunt build css  and js

--- src/main/java/com/aboutdata/web/listener/ServletListener.java
+++ src/main/java/com/aboutdata/web/listener/ServletListener.java
@@ -6,6 +6,11 @@
 package com.aboutdata.web.listener;
 
 import com.aboutdata.commons.application.ApplicationBean;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.io.InputStream;
+import javax.json.Json;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.annotation.WebListener;
@@ -15,34 +20,47 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.WebApplicationContext;
+
 /**
  *
  * @author youyou
  */
 @WebListener
 public class ServletListener extends ContextLoaderListener implements HttpSessionListener {
-    
+
     Logger logger = LoggerFactory.getLogger(ServletListener.class);
     private WebApplicationContext springContext;
-    
+
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         ServletContext sc = sce.getServletContext();
+
+        //设置version 从grunt打包到生产使用
+        try (InputStream is = sc.getResourceAsStream("/package.json")) {
+            ObjectMapper mapper = new ObjectMapper();
+            String version = mapper.readTree(is).path("version").asText();
+            sc.setAttribute("GruntVersion", version);
+            
+            logger.info("packeage.json version is {}", version);
+        } catch (IOException ex) {
+            //生产环境 打包部署需要
+            logger.error("packeage.json is not exist {}", ex);
+        }
         //获取spring上下文
         springContext = getCurrentWebApplicationContext();
         ApplicationBean appBean = (ApplicationBean) springContext.getBean("appBean");
         sc.setAttribute("appBean", appBean);
-        logger.info("config {}",appBean.getSystemConfig());
+        logger.info("config {}", appBean.getSystemConfig());
     }
-    
+
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
     }
-    
+
     @Override
     public void sessionCreated(HttpSessionEvent se) {
     }
-    
+
     @Override
     public void sessionDestroyed(HttpSessionEvent se) {
     }
---
+++ src/main/webapp/Gruntfile.js
@@ -0,0 +1,82 @@
+module.exports = function (grunt) {
+// 配置
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        clean: {
+            cleanoutput: {
+                files: [{
+                        src: 'build-<%= pkg.version %>/'
+                    }]
+            }
+        },
+        copy: {
+            fonts: {
+                files: [
+                    {expand: true, cwd: 'assets', src: ['fonts/**'], dest: 'build-<%= pkg.version %>/'}
+                ]
+            },
+            image: {
+                files: [
+                    {
+                        expand: true,
+                        flatten: false,
+                        cwd: 'assets',
+                        src: ['images/*.{png,jpg,jpeg,gif}'],
+                        dest: 'build-<%= pkg.version %>/'
+                    }
+                ]
+            },
+            js: {
+                //*.js 代表只复制js ** 包含复制目录
+                files: [
+                    {expand: true, cwd: 'assets', src: ['js/**'], dest: 'build-<%= pkg.version %>/'}
+                ]
+            },
+            css: {
+                files: [
+                    {expand: true, cwd: 'assets',
+                        src: [
+                            'css/animate.css',
+                            'css/bootstrap.css',
+                            'css/bootstrap.css.map',
+                            'css/font-awesome.min.css',
+                            'css/font.css',
+                            'css/simple-line-icons.css'],
+                        dest: 'build-<%= pkg.version %>/'}
+                ]
+            }
+        },
+        concat: {
+            css: {
+                src: ['assets/css/app.css'],
+                dest: 'build-<%= pkg.version %>/css/app.css'
+            }
+        },
+        cssmin: {
+            css: {
+                src: 'build-<%= pkg.version %>/css/app.css',
+                dest: 'build-<%= pkg.version %>/css/app-min.css'
+            }
+        },
+        uglify: {
+            main: {
+                files: [{
+                        expand: true,
+                        cwd: 'assets',
+                        src: ['js/*.js', '!js/*.min.js'],
+                        dest: 'build-<%= pkg.version %>'
+                    }]
+            }
+        }
+    });
+    //拷贝文件
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    //clean 插件
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    // 载入concat和uglify插件，分别对于合并和压缩
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-css');
+    // 注册任务
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.registerTask('default', ['clean', 'concat', 'cssmin', 'uglify', 'copy']);
+}; 
