8b07dd341945600a55d1f541e5e825499226d240
一名清官 <aboutdata@vip.qq.com>
2015-11-03 17:08:59 +0800

    从网络获取图片到本地 然后截取

--- src/main/java/com/aboutdata/service/bean/PhotosColorsServiceImpl.java
+++ src/main/java/com/aboutdata/service/bean/PhotosColorsServiceImpl.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import javax.annotation.Resource;
 import org.springframework.stereotype.Service;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
 import me.croma.image.AWTImage;
@@ -21,12 +22,14 @@ import me.croma.image.Color;
 import me.croma.image.ColorPicker;
 import me.croma.image.Image;
 import me.croma.image.KMeansColorPicker;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
- * 图片颜色截取
+ * @ 图片颜色截取
  *
  * @author Administrator
  */
@@ -44,8 +47,11 @@ public class PhotosColorsServiceImpl implements PhotosColorsService {
     @Override
     @Transactional
     public void generateColors(String photosID) {
+        Photos photos = photosDao.findOne(photosID);
         try {
-            File file = new File("D:\\workspace\\GitHub\\croma\\images\\lockbur-com.jpg");
+            File file = getImages("" + photos.getSource(), "/tmp/full" + photos.getWallhaven() + ".jpg");
+
+            //File file = new File("D:\\workspace\\GitHub\\croma\\images\\lockbur-com.jpg");
             Image img = new AWTImage(file);
 
             ColorPicker km = new KMeansColorPicker();
@@ -55,8 +61,6 @@ public class PhotosColorsServiceImpl implements PhotosColorsService {
 
             //6 取出6种颜色
             List<Color> list = km.getUsefulColors(img, 6);
-            Photos photos = new Photos();
-            photos.setId(photosID);
             for (Color c : list) {
                 PhotosColors colors = new PhotosColors();
                 colors.setBlue(c.getBlue());
@@ -71,5 +75,34 @@ public class PhotosColorsServiceImpl implements PhotosColorsService {
             logger.error("cut images color error inf :{}", ex);
         }
 
+    }
+
+    /**
+     * @param urlPath
+     * @param fileName 默认存放到/tmp/
+     * @return
+     */
+    public File getImages(String urlPath, String fileName) {
+        // System.out.println("urlPath: " + urlPath);
+        Connection conn = Jsoup
+                .connect(urlPath)
+                .userAgent(
+                        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.95 Safari/537.36");
+        try {
+            Connection.Response response = conn.ignoreContentType(true).execute();
+            if (response.statusCode() == 200) {
+                byte[] data = response.bodyAsBytes();
+                FileOutputStream outputStream = new FileOutputStream(fileName);
+                outputStream.write(data);
+                outputStream.close();
+                File sourceFile = new File(fileName);
+                return sourceFile;
+            } else {
+                logger.error("download {} 404 ", urlPath);
+            }
+        } catch (Exception e) {
+            logger.error("download error message: {}", e.getMessage());
+        }
+        return null;
     }
 }
